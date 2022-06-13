3bbd931184ca79514cd1378472df4c61c9444795
Shubham <shubhamshivaay67@gmail.com>
2022-06-14 00:24:49 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import PrescriptionForm from "./components/PrescriptionForm";
 import Prescription from "./components/prescription";
 
 function App() {
-  const uri = decodeURI(window.location.search.substring(1))
-    .replace(/"/g, '\\"')
-    .replace(/&/g, '","')
-    .replace(/=/g, '":"');
-  let formValues = {};
-  try {
-    formValues = JSON.parse('{"' + uri + '"}');
-  } catch {
-    // no data in form
-  }
+  const parseParams = () => {
+    let formValues = {};
+    const uri = decodeURI(window.location.search.substring(1))
+      .replace(/"/g, '\\"')
+      .replace(/&/g, '","')
+      .replace(/=/g, '":"');
+    try {
+      formValues = JSON.parse('{"' + uri + '"}');
+    } catch {
+      // no data in form
+    }
+    Object.keys(formValues).forEach((key) => {
+      formValues[key] = formValues[key].replace(/['"]+/g, "");
+    });
+    return formValues;
+  };
+  const formValues = parseParams();
+
   return Object.keys(formValues).length !== 0 ? (
     <Prescription {...formValues} />
   ) : (
